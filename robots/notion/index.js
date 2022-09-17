2184f0ba14eb21e280097d40a8b57da24e589b5f
João Vitor Novais <joaoeuno2011@gmail.com>
2022-09-17 20:09:07 +0000

    adicionando save state de conteudo da aplicação

--- robots/notion/index.js
+++ robots/notion/index.js
@@ -1,15 +1,15 @@
 const notion = require('../notion/functions');
+const state = require('../state/index');
 const page = require('../input');
 
 async function init() {
     try {
-        const idPage = await page(); 
-        const results = await notion.contentWithBlocks(idPage);
-        console.log(results);
+        //const idPage = await page(); 
+        const contents = await notion.contentWithBlocks("cdd92d0268714e52ac43cd7c6494390d");
+       await state.save(contents.results);
     } catch(err){
         console.error('Erro:',err);
     }
-    
 
 }
 
